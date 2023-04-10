fab1c3f056dc482ffd35af70776f1372944d9c3a
ErrorNoInternet <error.nointernet@gmail.com>
2023-04-10 21:20:41 +0800

    Add database trait

---
+++ src/database/mod.rs
@@ -0,0 +1,17 @@
+pub mod sqlite;
+
+use crate::minecraft;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait Database {
+    async fn new() -> Result<Self, String>
+    where
+        Self: Sized;
+    async fn add_server(server: minecraft::server::Server) -> Result<(), String>;
+    async fn add_player(player: minecraft::player::Player) -> Result<(), String>;
+    async fn add_targeted_server(server: minecraft::server::Server) -> Result<(), String>;
+    async fn add_targeted_player(player: minecraft::player::Player) -> Result<(), String>;
+
+    // TODO: Add CLI configuration options
+}
