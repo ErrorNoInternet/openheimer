67d725679ac9e8259b0a94a9a3b14207f14b9940
ErrorNoInternet <error.nointernet@gmail.com>
2023-05-03 19:03:58 +0800

    Add basic argument parser

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,26 @@ mod database;
 mod logger;
 mod minecraft;
 
-use logger::log_message;
+use clap::{Arg, ArgAction, Command};
 use logger::LogMessageType::*;
 
 fn main() {
-    log_message(
+    let matches = Command::new("openheimer")
+        .about("The final word in Minecraft server scanners")
+        .arg(
+            Arg::new("verbosity")
+                .short('v')
+                .long("verbose")
+                .help("Sets the level of verbosity")
+                .action(ArgAction::Count),
+        )
+        .get_matches();
+
+    let logger = logger::Logger {
+        verbosity: matches.get_count("verbosity") as usize,
+    };
+    logger.log_message(
         Information,
         &format!("Starting OpenHeimer v{}...", env!("CARGO_PKG_VERSION")),
-    )
+    );
 }
