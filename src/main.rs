a5513b07e079d4df865c7ed5cb2809c45cddde36
ErrorNoInternet <error.nointernet@gmail.com>
2023-05-03 18:37:53 +0800

    Print startup message

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+mod configuration;
 mod database;
+mod logger;
 mod minecraft;
 
+use logger::log_message;
+use logger::LogMessageType::*;
+
 fn main() {
-    println!("hello, openheimer!")
+    log_message(
+        Information,
+        &format!("Starting OpenHeimer v{}...", env!("CARGO_PKG_VERSION")),
+    )
 }
