90fdb6de632d58c141fcfe26dcb624b7c024b0c2
ErrorNoInternet <error.nointernet@gmail.com>
2023-06-20 15:29:02 +0800

    Read and parse configuration

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ mod logger;
 mod minecraft;
 
 use clap::{Arg, ArgAction, Command};
+use configuration::Configuration;
 use logger::LogMessageType::*;
+use std::str::FromStr;
 
 fn main() {
     let matches = Command::new("openheimer")
@@ -26,4 +28,33 @@ fn main() {
         Information,
         &format!("Starting OpenHeimer v{}...", env!("CARGO_PKG_VERSION")),
     );
+
+    let mut configuration_string;
+    if std::path::Path::new("openheimer.toml").exists() {
+        logger.log_message(Verbose1, "Reading configuration from openheimer.toml...");
+        configuration_string = match std::fs::read_to_string("openheimer.toml") {
+            Ok(file_data) => file_data,
+            Err(error) => {
+                logger.log_error("Unable to read openheimer.toml", &error);
+                return;
+            }
+        };
+    } else {
+        logger.log_message(
+            Verbose1,
+            "Saving default configuration to openheimer.toml...",
+        );
+        let defualt_configuration_string = Configuration::default().to_string();
+        configuration_string = defualt_configuration_string.clone();
+        match std::fs::write("openheimer.toml", &defualt_configuration_string) {
+            Ok(_) => (),
+            Err(error) => {
+                logger.log_error("Unable to write to openheimer.toml", &error);
+                return;
+            }
+        };
+    }
+
+    logger.log_message(Verbose3, "Parsing configuration from string...");
+    let configuration = Configuration::from_str(&configuration_string);
 }
