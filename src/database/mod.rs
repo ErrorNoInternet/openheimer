6f348b7df972a4c3e549360d49ac0dd9be7512e3
ErrorNoInternet <errornointernet@envs.net>
2023-10-16 13:41:01 +0800

    Fix Database trait

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -3,15 +3,29 @@ pub mod sqlite;
 use crate::minecraft;
 use async_trait::async_trait;
 
+pub enum DatabaseError {
+    SqlxPoolCreateFailed(sqlx::Error),
+    SqlxAcquirePoolFailed(sqlx::Error),
+    SqlxInsertQueryFailed(sqlx::Error),
+}
+
 #[async_trait]
 pub trait Database {
-    async fn new(location: String) -> Result<Self, String>
+    async fn new(location: String) -> Result<Self, DatabaseError>
     where
         Self: Sized;
-    async fn add_server(server: minecraft::server::Server) -> Result<(), String>;
-    async fn add_player(player: minecraft::player::Player) -> Result<(), String>;
-    async fn add_targeted_server(server: minecraft::server::Server) -> Result<(), String>;
-    async fn add_targeted_player(player: minecraft::player::Player) -> Result<(), String>;
+    async fn add_server(&mut self, server: &minecraft::server::Server)
+        -> Result<(), DatabaseError>;
+    async fn add_player(&mut self, player: &minecraft::player::Player)
+        -> Result<(), DatabaseError>;
+    async fn add_targeted_server(
+        &mut self,
+        server: &minecraft::server::Server,
+    ) -> Result<(), DatabaseError>;
+    async fn add_targeted_player(
+        &mut self,
+        player: &minecraft::player::Player,
+    ) -> Result<(), DatabaseError>;
 
     // TODO: Add CLI configuration options
 }
