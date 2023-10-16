ca44bd6668b13240cb8948eb2185f8a54c1997f9
ErrorNoInternet <errornointernet@envs.net>
2023-10-16 13:41:19 +0800

    Add basic trait functions for sqlite3 database

--- src/database/sqlite.rs
+++ src/database/sqlite.rs
@@ -1,42 +1,86 @@
-use super::Database;
+use super::{Database, DatabaseError};
 use crate::minecraft::{player::Player, server::Server};
 use async_trait::async_trait;
 use sqlx::sqlite::SqlitePoolOptions;
 
 pub struct SqliteDatabase {
-    database: sqlx::SqlitePool,
+    pool: sqlx::SqlitePool,
 }
 
 #[async_trait]
 impl Database for SqliteDatabase {
-    async fn new(location: String) -> Result<Self, String> {
-        let database = match SqlitePoolOptions::new()
+    async fn new(location: String) -> Result<Self, DatabaseError> {
+        let pool = match SqlitePoolOptions::new()
             .connect(format!("sqlite://{location}?mode=rwc").as_str())
             .await
         {
-            Ok(database) => database,
-            Err(error) => return Err(error.to_string()),
+            Ok(pool) => pool,
+            Err(error) => return Err(DatabaseError::SqlxPoolCreateFailed(error)),
         };
-        Ok(SqliteDatabase { database })
+        Ok(SqliteDatabase { pool })
     }
 
-    async fn add_server(server: Server) -> Result<(), String> {
-        // TODO: Add server
-        Ok(())
+    async fn add_server(&mut self, server: &Server) -> Result<(), DatabaseError> {
+        let serialized_server = serde_json::to_string(server).unwrap();
+        let mut connection = match self.pool.acquire().await {
+            Ok(connection) => connection,
+            Err(error) => return Err(DatabaseError::SqlxAcquirePoolFailed(error)),
+        };
+        match sqlx::query("INSERT INTO servers ( serialized_server ) VALUES ( ?1 )")
+            .bind(serialized_server)
+            .execute(&mut *connection)
+            .await
+        {
+            Ok(_) => Ok(()),
+            Err(error) => Err(DatabaseError::SqlxInsertQueryFailed(error)),
+        }
     }
 
-    async fn add_player(player: Player) -> Result<(), String> {
-        // TODO: Add player
-        Ok(())
+    async fn add_player(&mut self, player: &Player) -> Result<(), DatabaseError> {
+        let serialized_player = serde_json::to_string(player).unwrap();
+        let mut connection = match self.pool.acquire().await {
+            Ok(connection) => connection,
+            Err(error) => return Err(DatabaseError::SqlxAcquirePoolFailed(error)),
+        };
+        match sqlx::query("INSERT INTO players ( serialized_player ) VALUES ( ?1 )")
+            .bind(serialized_player)
+            .execute(&mut *connection)
+            .await
+        {
+            Ok(_) => Ok(()),
+            Err(error) => Err(DatabaseError::SqlxInsertQueryFailed(error)),
+        }
     }
 
-    async fn add_targeted_server(server: Server) -> Result<(), String> {
-        // TODO: Add server
-        Ok(())
+    async fn add_targeted_server(&mut self, server: &Server) -> Result<(), DatabaseError> {
+        let serialized_server = serde_json::to_string(server).unwrap();
+        let mut connection = match self.pool.acquire().await {
+            Ok(connection) => connection,
+            Err(error) => return Err(DatabaseError::SqlxAcquirePoolFailed(error)),
+        };
+        match sqlx::query("INSERT INTO targeted_servers ( serialized_server ) VALUES ( ?1 )")
+            .bind(serialized_server)
+            .execute(&mut *connection)
+            .await
+        {
+            Ok(_) => Ok(()),
+            Err(error) => Err(DatabaseError::SqlxInsertQueryFailed(error)),
+        }
     }
 
-    async fn add_targeted_player(player: Player) -> Result<(), String> {
-        // TODO: Add player
-        Ok(())
+    async fn add_targeted_player(&mut self, player: &Player) -> Result<(), DatabaseError> {
+        let serialized_player = serde_json::to_string(player).unwrap();
+        let mut connection = match self.pool.acquire().await {
+            Ok(connection) => connection,
+            Err(error) => return Err(DatabaseError::SqlxAcquirePoolFailed(error)),
+        };
+        match sqlx::query("INSERT INTO targeted_players ( serialized_player ) VALUES ( ?1 )")
+            .bind(serialized_player)
+            .execute(&mut *connection)
+            .await
+        {
+            Ok(_) => Ok(()),
+            Err(error) => Err(DatabaseError::SqlxInsertQueryFailed(error)),
+        }
     }
 }
