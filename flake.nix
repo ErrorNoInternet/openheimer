eaceaf3f12f1eb835aa3069181790ac1b9423362
ErrorNoInternet <errornointernet@envs.net>
2024-01-30 02:14:53 +0800

    ci: release workflow overhaul

--- flake.nix
+++ flake.nix
@@ -30,9 +30,9 @@
 
         rust = pkgs.rust-bin.nightly.latest.default.override {
           targets = [
+            "x86_64-pc-windows-gnu"
             "x86_64-unknown-linux-gnu"
             "x86_64-unknown-linux-musl"
-            "x86_64-pc-windows-gnu"
           ];
           extensions = [
             "rust-src"
