b4b9267e57e0f3752d86e7bb89a47d1512836855
ErrorNoInternet <errornointernet@envs.net>
2024-01-29 19:43:57 +0800

    fix: file logging

--- src/core.rs
+++ src/core.rs
@@ -1,12 +1,18 @@
 use crate::{arguments::Arguments, configuration::Configuration, metadata};
 use std::{process::exit, str::FromStr};
 use tracing::{debug, info, trace, warn};
+use tracing_appender::non_blocking::NonBlocking;
 use tracing_subscriber::prelude::*;
 
 pub fn main(arguments: &Arguments) {
     let (is_default, options) = get_options(arguments);
 
-    set_up_logging(options.clone());
+    let binding = options.clone();
+    let (file_appender, _guard) = tracing_appender::non_blocking(tracing_appender::rolling::daily(
+        binding.logger.directory,
+        binding.logger.prefix,
+    ));
+    set_up_logging(file_appender);
 
     info!("openheimer {}", metadata::format());
     if is_default {
@@ -39,11 +45,7 @@ fn get_options(arguments: &Arguments) -> (bool, Configuration) {
     }
 }
 
-fn set_up_logging(options: Configuration) {
-    let (file_appender, _guard) = tracing_appender::non_blocking(tracing_appender::rolling::daily(
-        options.logger.directory,
-        options.logger.prefix,
-    ));
+fn set_up_logging(file_appender: NonBlocking) {
     let file_layer = tracing_subscriber::fmt::layer()
         .with_file(true)
         .with_line_number(true)
@@ -63,13 +65,19 @@ fn set_up_logging(options: Configuration) {
 
 #[cfg(test)]
 mod test {
-    use super::set_up_logging;
     use crate::configuration::Configuration;
+    use super::set_up_logging;
     use tracing::{debug, error, info, trace, warn};
 
     #[test]
     fn log_messages() {
-        set_up_logging(Configuration::default());
+        let configuration = Configuration::default();
+        let (file_appender, _guard) =
+            tracing_appender::non_blocking(tracing_appender::rolling::daily(
+                configuration.logger.directory,
+                configuration.logger.prefix,
+            ));
+        set_up_logging(file_appender);
         error!("h");
         warn!("e");
         info!("l");
